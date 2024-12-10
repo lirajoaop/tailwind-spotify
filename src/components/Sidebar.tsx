938da9855fa5323413e0de7e57b523e6636a5c36
lirajoaop <joaopedroababa132@gmail.com>
2024-12-09 22:21:55 -0300

    start redesign from sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,27 +1,24 @@
 import {Home as HomeIcon, Library, Search } from 'lucide-react'
+import Image from 'next/image'
 import React from 'react'
 
 const Sidebar = () => {
   return (
     <aside className="w-72 bg-zinc-950 p-6">
-    <div className="flex items-center gap-2">
-      <div className="w-3 h-3 bg-red-500 rounded-full"/>
-      <div className="w-3 h-3 bg-yellow-500 rounded-full"/>
-      <div className="w-3 h-3 bg-green-500 rounded-full"/>
-    </div>
+    <Image src='/spotify-logo.png' alt='Spotify Logo' width={164} height={49} />
 
     <nav className="space-y-5 mt-10">
-      <a href="" className="flex items-center gap-3 text-sm font-semibold text-zinc-200">
-        <HomeIcon />
-        Home
+      <a href="" className="flex items-center gap-5 text-sm font-semibold text-zinc-200">
+        <HomeIcon size={24} />
+        <p className='font-bold text-[19px]'>Home</p>
       </a>
-      <a href="" className="flex items-center gap-3 text-sm font-semibold text-zinc-200">
-        <Search />
-        Search
+      <a href="" className="flex items-center gap-5 text-sm font-semibold text-zinc-200">
+        <Search size={24} />
+        <p className='font-bold text-[19px]'>Search</p>
       </a>
-      <a href="" className="flex items-center gap-3 text-sm font-semibold text-zinc-200">
-        <Library />
-        Your Library
+      <a href="" className="flex items-center gap-5 text-sm font-semibold text-zinc-200">
+        <Library size={24} />
+        <p className='font-bold text-[19px]'>Your Library</p>
       </a>
     </nav>
 
